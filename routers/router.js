3612556e804812e028e89b83bc8321bef3bb0cde
codewithmahadihasan <73072248+codewithmahadihasan@users.noreply.github.com>
2025-08-29 01:04:53 +0600

    update settings api

--- routers/router.js
+++ routers/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const image_router = require('../modules/image/image_router');  // Adjust the path if necessary
-const initial_route = require('../modules/hooks/initial_route'); // Adjust the path if necessary
-const auth_router = require('../modules/auth/auth_router'); // Adjust the path if necessary
-const item_router = require('../modules/items/item_router'); // Adjust the path if necessary
+const image_router = require('../modules/image/image_router');
+const initial_route = require('../modules/hooks/initial_route');
+const auth_router = require('../modules/auth/auth_router');
+const item_router = require('../modules/items/item_router');
+const settings_router = require('../modules/settings/settings_router');
 
 // Define module routes
 const modulesRoutes = [
@@ -22,6 +23,10 @@ const modulesRoutes = [
       {
             path: '/items',      // Items routes
             route: item_router,
+      },
+      {
+            path: '/settings',      // Settings routes
+            route: settings_router,
       },
 ];
 
