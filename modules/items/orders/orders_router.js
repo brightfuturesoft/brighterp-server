22f402c8fbbcaba541f6b8c17a6a0462aeb42122
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-09-03 15:29:13 +0600

    create API for getting last transaction +1 for creating transaction numbers

--- modules/items/orders/orders_router.js
+++ modules/items/orders/orders_router.js
@@ -7,11 +7,13 @@ const {
   get_orders,
   update_order,
   delete_order,
+  get_transaction_id,
 } = require("./orders_module");
 
 router.post("/create-order", check_user, create_order);
 router.get("/get-orders", check_user, get_orders);
 router.put("/update-order", check_user, update_order);
 router.delete("/delete-order", check_user, delete_order);
+router.get("/get-transaction-id", check_user, get_transaction_id);
 
 module.exports = router;
