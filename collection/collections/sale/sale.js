c53b37480bc7df645c7a14d3b2492fdc44bc2861
ripon4521 <riponakondo4521@gmail.com>
2025-09-24 18:39:17 +0600

    create quations

--- collection/collections/sale/sale.js
+++ collection/collections/sale/sale.js
@@ -1,5 +1,6 @@
 const { client } = require("../../uri");
 
 const direct_sales_collection = client.db('sale').collection("direct_sale");
+const questions_collection = client.db('sale').collection("questions");
 
-module.exports = {direct_sales_collection};
+module.exports = {direct_sales_collection, questions_collection};
--- modules/sale/sale_router.js
+++ modules/sale/sale_router.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const newsletter_router = require('./newsletter/newsletter_route');
+const newsletter_router = require('./direct_sale/direct_sale_route');
+const quataions_router = require('./quataions/quations_route');
 
 
 const modules_Routes = [
       {
             path: '/direct_sale',        
             route: newsletter_router,
+      },{
+            path:"/quataions",
+            route:quataions_router
       }
 ];
 
