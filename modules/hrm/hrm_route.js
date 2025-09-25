3651fe6e7f3b6e35add885e04c609e2c81afcfd1
maria-jahan20 <mariajahan4596@gmail.com>
2025-09-25 18:10:51 +0600

    employee-server

--- modules/hrm/hrm_route.js
+++ modules/hrm/hrm_route.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const employees_router=require('./employees/employees_route')
+const employees_router=require('./employees/employees_route');
+const attendance_router=require('./attendance/attendance_route')
 
 // Define module routes
 const modulesRoutes = [
       {
             path: '/employees',
             route: employees_router,
-      }
+      },
+      {
+            path: '/attendance',
+            route: attendance_router
+      },
 ];
 
 // Attach each route to the main router
